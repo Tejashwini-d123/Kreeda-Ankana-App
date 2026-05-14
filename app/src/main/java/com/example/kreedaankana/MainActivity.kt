cd2d23d4dc0b97f4e84fea95617c17dea70ce5f3
Tejashwini-d123 <tejashwini11604@gmail.com>
2026-05-14 07:48:15 +0530

    test update

--- app/src/main/java/com/example/kreedaankana/MainActivity.kt
+++ app/src/main/java/com/example/kreedaankana/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
 
                     // ✅ APP NAVIGATION
                     AppNavigation()
-                }
+                }// test commit
             }
         }
     }
